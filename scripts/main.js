38a4869df75665089a78763877383f126af3d526
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2021-07-10 02:31:25 -0700

    Fix Bug on light theme & Add Contact

--- scripts/main.js
+++ scripts/main.js
@@ -7,32 +7,61 @@
  * @brief auto text effect for id who
  */
 async function whoTextTypingEffect() {
-    var data = ['Student !', 'Programmer !'];
-    var who  = $('#who .content');
-    var len  = data.length;
-    var wait = (ms) => new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    })
+  var data = ["Student !", "Programmer !"];
+  var who = $("#who .content");
+  var len = data.length;
+  var wait = (ms) =>
+    new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
 
-    for(let i = 0; i < len; i = ((i + 1) == len ? 0 : i + 1) ) {
-        for(let j = 0; j < data[i].length; ++j) {
-            who.text(data[i].substring(0, j + 1));
-            await wait(200);
-        }
-
-        await wait(1000);
-
-        for(let j = data[i].length - 1; j > 0; --j) {
-            who.text(data[i].substring(0, j - 1));
-            await wait(200);
-        }
+  for (let i = 0; i < len; i = i + 1 == len ? 0 : i + 1) {
+    for (let j = 0; j < data[i].length; ++j) {
+      who.text(data[i].substring(0, j + 1));
+      await wait(200);
     }
+
+    await wait(1000);
+
+    for (let j = data[i].length - 1; j > 0; --j) {
+      who.text(data[i].substring(0, j - 1));
+      await wait(200);
+    }
+  }
+}
+
+/**
+ * @brief Handeler for Form Submit
+ */
+function formSubmit(evt) {
+  var subj = $("#contactsubject").val();
+  var message = $("#contactmessage").val();
+
+  if (subj.length == 0) {
+    alert("Subject is required");
+    return false;
+  }
+
+  if (message.length == 0) {
+    alert("Message is required");
+    return false;
+  }
+
+  $(location).attr(
+    "href",
+    `mailto:srilakshmikanthanp@gmail.com?subject=${encodeURIComponent(
+      subj
+    )}&body=${encodeURIComponent(message)}`
+  );
+
+  return false;
 }
 
 /**
  * Initilize the Page
  */
 $(function () {
-    whoTextTypingEffect();
-    feather.replace();
+  whoTextTypingEffect();
+  $("#contactsubmit").on("click", formSubmit);
+  feather.replace();
 });
