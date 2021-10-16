908892b268196d369e5d4b4d5bbd92d63e0d620c
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2021-10-16 16:55:02 +0530

    Update Website

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,7 +8,7 @@
  */
 async function textTypingEffect() {
   var data = ["I'm a Student !", " I'm a Programmer !"];
-  var who = $("section#home p:last-child");
+  var who = $("section#intro p:last-child");
   var len = data.length;
 
   var wait = (ms) => new Promise((resolve) => {
@@ -16,18 +16,27 @@ async function textTypingEffect() {
   });
 
   for (let i = 0; i < len; i = i + 1 == len ? 0 : i + 1) {
-    for (let j = 0; j < data[i].length; ++j) {
+    for (let j = 6; j < data[i].length; ++j) {
       who.text(data[i].substring(0, j + 1));
       await wait(300);
     }
 
     await wait(1000);
 
-    for (let j = data[i].length - 1; j > 0; --j) {
+    for (let j = data[i].length - 1; j > 6; --j) {
       who.text(data[i].substring(0, j - 1));
       await wait(200);
     }
   }
+}
+
+/**
+ * @brief projects link click
+ */
+function projectsTrClick(evt) {
+  var link = $(evt.currentTarget).data('link');
+  console.log(evt.currentTarget);
+  window.open(link);
 }
 
 /**
@@ -53,4 +62,5 @@ $(function() {
 
   // event listeners
   $("a[href^='#']").on("click", smoothScroll);
+  $("section#projects table tr").on('click', projectsTrClick);
 });
