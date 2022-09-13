bb8f832c5680b221ad21a95fd0f188ed15846a4b
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2022-09-13 22:22:22 +0530

    Init: Initialize the yhe Folder Structure

---
+++ src/components/index.tsx
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Sri Lakshmi Kanthan P
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import Footer from "./Footer";
+import Header from "./Header";
+import Image from "./Image";
+import Project from "./Project";
+import Tool from "./Tool";
+
+export {
+  Footer,
+  Header,
+  Image,
+  Project,
+  Tool
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 Sri Lakshmi Kanthan P
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import React from "react";
