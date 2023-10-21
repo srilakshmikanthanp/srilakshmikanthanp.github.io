2410a4d08328b681824a2031f2c0eb3bb3de5a3d
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2023-10-21 17:12:11 +0530

    Update: Make the intro professional

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -42,7 +42,7 @@ const Footer = styled.footer`
 
 export default function Intro() {
   // Type Writer words
-  const words = ['I am a Student !', 'I am a Programmer !', 'I am a Stupid 😂'];
+  const words = ['I am a Student !', 'I am a Programmer !'];
 
   // Type writer
   const typeWriter = (
